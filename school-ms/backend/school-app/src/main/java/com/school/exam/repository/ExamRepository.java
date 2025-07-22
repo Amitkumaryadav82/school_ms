6fbc65a532aff7ef83233ec47496e3c1ae0a1ed9
Amitkumaryadav82 <amitkumaryadav82@gmail.com>
2025-07-22 16:10:28 +0530

    All Exam management functionlities are working correctly now

--- school-ms/backend/school-app/src/main/java/com/school/exam/repository/ExamRepository.java
+++ school-ms/backend/school-app/src/main/java/com/school/exam/repository/ExamRepository.java
@@ -1,4 +1,11 @@
+
 package com.school.exam.repository;
+
+import com.school.exam.model.Exam;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
 import com.school.exam.model.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +13,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+    @Override
+    @EntityGraph(attributePaths = "classes")
+    List<Exam> findAll();
 }
